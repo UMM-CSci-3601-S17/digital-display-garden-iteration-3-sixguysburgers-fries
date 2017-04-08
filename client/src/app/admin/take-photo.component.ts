c22bc89d923f249070450ed5ff3ef1036678c8a9
holma198 <holma198@morris.umn.edu>
2017-04-07 22:26:49 -0500

    Add a camera app into the main app itself, with a preview window, this will need to be tested on a device with a webcam, an alternative method(Launch camera external camera app) will be provided in another branch.
    #5
    #15

--- client/src/app/admin/take-photo.component.ts
+++ client/src/app/admin/take-photo.component.ts
@@ -5,6 +5,8 @@ import { AdminService } from './admin.service';
 @Component({
     selector: 'take-photo-component',
     templateUrl: 'take-photo.component.html',
+    styles: ['video.preview-window { display: block;margin: 0 auto; }',
+     'button.capture { display: block;margin: 0 auto; }'],
 })
 
 export class TakePhotoComponent implements OnInit {
