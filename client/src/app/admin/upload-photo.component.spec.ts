7ae2f8fb4b166da90ade15fd00df3d922f76f073
hamme503 <hamme503@morris.umn.edu>
2017-04-09 17:54:00 -0500

    Add .spec file with imports for testing image uploads
    Issue #14

---
+++ client/src/app/admin/upload-photo.component.spec.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by hamme503 on 4/9/17.
+ */
+import { ComponentFixture, async, inject} from "@angular/core/testing";
+import { UploadPhotoComponent } from "./upload-photo.component";
+import { ImageUploadComponent } from "./image-upload.component";
+import { Observable } from "rxjs";
+import { FormsModule } from "@angular/forms";
