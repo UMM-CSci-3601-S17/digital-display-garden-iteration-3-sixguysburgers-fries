4b250dc91c09aba04668128813879a5254aa3144
hamme503 <hamme503@morris.umn.edu>
2017-04-11 20:52:12 -0500

    Added existing upload timestamp code to ImageHandler
    Issue #2

--- server/src/main/java/umm3601/digitalDisplayGarden/ImageHandler.java
+++ server/src/main/java/umm3601/digitalDisplayGarden/ImageHandler.java
@@ -1,5 +1,7 @@
 package umm3601.digitalDisplayGarden;
 
+import org.joda.time.DateTime;
+
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.InputStream;
@@ -9,6 +11,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import java.awt.Image;
+import java.util.Date;
+import java.util.Formatter;
+
 /**
  * Created by hamme503 on 4/6/17.
  */
@@ -28,6 +33,27 @@ public class ImageHandler {
         } catch (IOException e) {
         }
         return image;
+    }
+
+    public static String getAvailableUploadId(){
+
+        StringBuilder sb = new StringBuilder();
+        // Send all output to the Appendable object sb
+        Formatter formatter = new Formatter(sb);
+
+        java.util.Date juDate = new Date();
+        DateTime dt = new DateTime(juDate);
+
+        int day = dt.getDayOfMonth();
+        int month = dt.getMonthOfYear();
+        int year = dt.getYear();
+        int hour = dt.getHourOfDay();
+        int minute = dt.getMinuteOfHour();
+        int seconds = dt.getSecondOfMinute();
+
+        formatter.format("%d-%02d-%02d %02d:%02d:%02d", year, month, day, hour, minute, seconds);
+        return sb.toString();
+
     }
 
 
