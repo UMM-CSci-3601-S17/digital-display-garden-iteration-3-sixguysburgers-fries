6a631b17c6cd464907f4c6189ce25e7a645f49b8
hamme503 <hamme503@morris.umn.edu>
2017-04-12 22:40:28 -0500

    Attempt to add fields to represent gathered form data
    Issue #2

--- client/src/app/admin/upload-photo.component.ts
+++ client/src/app/admin/upload-photo.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AdminService } from './admin.service';
 import {ImageUploadComponent} from "./image-upload.component";
 
@@ -11,12 +11,18 @@ import {ImageUploadComponent} from "./image-upload.component";
 export class UploadPhotoComponent implements OnInit {
 
     @ViewChild('iu') iu;
+    @ViewChild('nameInput') nameInput: ElementRef;
 
+
+    inputName: string;
     filename:string;
     uploadAttempted:boolean = false;
 
-    handleUpload(){
-        this.iu.upload().subscribe(
+
+    handleUpload() {
+        this.inputName = this.nameInput.nativeElement.value;
+        console.log("inputName = " + this.inputName);
+        this.iu.upload(this.nameInput).subscribe(
             response => {
                 this.filename = response.json();
                 this.uploadAttempted = true;
