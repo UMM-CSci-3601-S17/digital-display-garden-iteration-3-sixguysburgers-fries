a7cae0bf09a57ea7332ab3745c3e88c0b44a99c5
Philip Blaskowski <blask017@morris.umn.edu>
2017-04-14 01:39:18 -0500

    Changed route endpoints to try and fix our droplet issues

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -15,15 +15,13 @@ import {UploadPhotoComponent} from "./admin/upload-photo.component";
 // Route Configuration
 export const routes: Routes = [
     { path: '', component: PlantListComponent },
-    { path: 'plants/:plantID', component: PlantComponent },
+    { path: 'plants/:plantID', component: BedComponent },
     { path: 'admin', component: AdminComponent},
     { path: 'admin/exportData', component: ExportComponent},
     { path: 'admin/importData', component: ImportComponent},
     { path: 'bed/:gardenLocation', component: BedComponent },
     { path: 'admin/charts', component: GraphComponent},
     { path: 'admin/updateData' , component: UpdateComponent},
-    { path: 'bed/:gardenLocation', component: BedComponent },
-    { path: 'bed/:gardenLocation', component: BedComponent },
     { path: 'admin/uploadPhoto', component: UploadPhotoComponent },
 
 ];
